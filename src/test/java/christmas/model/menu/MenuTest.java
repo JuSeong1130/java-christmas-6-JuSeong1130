b0e5d8e27c5cb96335dac4f955dafd20396de149
JuSeong1130 <jsmoon1130@naver.com>
2023-11-12 16:07:50 +0900

    test(MenuTest) : 같은 이름인지 기능 테스트
    1. 같다면 True
    2. 다르다면 False

---
+++ src/test/java/christmas/model/menu/MenuTest.java
@@ -0,0 +1,30 @@
+package christmas.model.menu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MenuTest {
+
+    @Test
+    @DisplayName("입력된 이름과 Menu가 가지고 있는 이름이 같다면 True")
+    void 입렫된_이름_같다면_True() {
+        // given
+        Menu menu = new Menu("테스트", 0);
+        String menuName = "테스트";
+        // when then
+        Assertions.assertThat(menu.isEqualName(menuName)).isTrue();
+    }
+
+    @Test
+    @DisplayName("입력된 이름과 Menu가 가지고 있는 이름이 다르다면 False")
+    void 입렫된_이름_다르다면_False() {
+        // given
+        Menu menu = new Menu("테스트", 0);
+        String menuName = "테스트1";
+        // when then
+        Assertions.assertThat(menu.isEqualName(menuName)).isFalse();
+    }
+}
