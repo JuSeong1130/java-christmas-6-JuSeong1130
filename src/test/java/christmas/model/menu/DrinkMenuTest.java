89a5eb31f47ea295f777551ea338232de1727205
JuSeong1130 <jsmoon1130@naver.com>
2023-11-12 15:56:03 +0900

    test(DrinkMenuTest) : 음료 데이터 입력 및 음료 메뉴 구하기 기능 테스트
    1. Enum에서 Menu로 잘 변환 됬는지 확인
    2. 드링크 가격의 값이 0 보다 이상인지 확인

---
+++ src/test/java/christmas/model/menu/DrinkMenuTest.java
@@ -0,0 +1,34 @@
+package christmas.model.menu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DrinkMenuTest {
+
+    @Test
+    @DisplayName("음료가 Menu객체로 잘 변환 됬는지 테스트")
+    void 음료_변환_테스트() {
+        // given
+        List<Menu> drinkMenus = DrinkMenu.getDrinkMenus();
+        // when, then
+        for(DrinkMenu dessertMenu : DrinkMenu.values()) {
+            boolean hasMenu = drinkMenus.stream()
+                    .anyMatch(menu -> menu.isEqualName(dessertMenu.getMenuName())
+                            && menu.isEqualPrice(dessertMenu.getPrice()));
+            Assertions.assertThat(hasMenu).isTrue();
+        }
+    }
+
+    @Test
+    @DisplayName("음료 가격이 모두 0원 이상인지 테스트")
+    void 음료_가격_테스트() {
+        for(DrinkMenu drinkMenu : DrinkMenu.values()) {
+            boolean isGraterThanZero = drinkMenu.getPrice() > 0;
+            Assertions.assertThat(isGraterThanZero).isTrue();
+        }
+    }
+}
