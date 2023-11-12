f1ca80e624823102d41738aeab8f57015f445936
JuSeong1130 <jsmoon1130@naver.com>
2023-11-12 18:26:01 +0900

    feat(OrderTest) : Order 생성 기능 테스트
    1. 20개 초과시 예외
    2. 중복 상품 주문시 예외
    3. 음료만 주문시 예외
    4. 위 3가지가 아니라면 생성 성공

---
+++ src/test/java/christmas/model/order/OrderTest.java
@@ -0,0 +1,75 @@
+package christmas.model.order;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.model.menu.Menu;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+class OrderTest {
+
+    @Test
+    @DisplayName("중복된 OrderItem이 있다면 Order 생성 시 예외 발생")
+    void 중복된_OrderItem_있다면_예외() {
+        // given
+        Menu menu = new Menu("티본스테이크", 3000);
+        Menu otherMenu = new Menu("티본스테이크", 3000);
+
+        OrderItem orderItem = new OrderItem(menu, 1);
+        OrderItem otherOrderItem = new OrderItem(otherMenu, 1);
+        List<OrderItem> orderItems = List.of(orderItem, otherOrderItem);
+        // when then
+        Assertions.assertThatThrownBy(() -> new Order(orderItems, LocalDateTime.now()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("메뉴의 총 수량이 20개가 초과된다면 Order 생성 시 예외 발생")
+    void 총_수량이_20개_초과_시_예외() {
+        // given
+        Menu menu = new Menu("샴페인", 3000);
+        Menu otherMenu = new Menu("티본스테이크", 3000);
+
+        OrderItem orderItem = new OrderItem(menu, 10);
+        OrderItem otherOrderItem = new OrderItem(otherMenu, 11);
+        List<OrderItem> orderItems = List.of(orderItem, otherOrderItem);
+        // when then
+        Assertions.assertThatThrownBy(() -> new Order(orderItems, LocalDateTime.now()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("OrderItem이 드링크 메뉴만이라면 Order 생성 시 예외 발생")
+    void 음료_메뉴만_주문_시_예외() {
+        // given
+        Menu menu = new Menu("레드와인", 60000);
+        Menu otherMenu = new Menu("제로콜라", 3000);
+
+        OrderItem orderItem = new OrderItem(menu, 1);
+        OrderItem otherOrderItem = new OrderItem(otherMenu, 1);
+        List<OrderItem> orderItems = List.of(orderItem, otherOrderItem);
+        // when then
+        Assertions.assertThatThrownBy(() -> new Order(orderItems, LocalDateTime.now()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("총 수량 20이하이며 중복으로 상품을 선택하지 않고 음료만 주문하지 않는다면 Order 생성 성공 ")
+    void Order_예외_없이_생성() {
+        // given
+        Menu menu = new Menu("티본스테이크", 60000);
+        Menu otherMenu = new Menu("제로콜라", 3000);
+
+        OrderItem orderItem = new OrderItem(menu, 1);
+        OrderItem otherOrderItem = new OrderItem(otherMenu, 1);
+        List<OrderItem> orderItems = List.of(orderItem, otherOrderItem);
+        // when then
+        Assertions.assertThatCode(() -> new Order(orderItems, LocalDateTime.now()))
+                .doesNotThrowAnyException();
+    }
+}
