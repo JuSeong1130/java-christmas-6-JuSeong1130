4b219f2a1a5fec67f6d21f189ed71fae40698877
JuSeong1130 <jsmoon1130@naver.com>
2023-11-12 18:25:23 +0900

    feat(Order) : Order 생성 기능 구현

---
+++ src/main/java/christmas/model/order/Order.java
@@ -0,0 +1,57 @@
+package christmas.model.order;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Order {
+
+    private List<OrderItem> orderItems;
+    private LocalDateTime orderTime;
+
+    public Order(List<OrderItem> orderItems, LocalDateTime orderTime) {
+        validate(orderItems, orderTime);
+        this.orderItems = orderItems;
+        this.orderTime = orderTime;
+    }
+
+    private void validate(List<OrderItem> orderItems, LocalDateTime orderTime) {
+        validateOrderQuantity(orderItems);
+        validateDuplication(orderItems);
+        validateOnlyDrinkMenu(orderItems);
+    }
+
+    //OrderItems?
+    private void validateOrderQuantity(List<OrderItem> orderItems) {
+        long totalQuantity = orderItems.stream()
+                .mapToInt(OrderItem::getQuantity)
+                .sum();
+        if(totalQuantity > 20) {
+            throw new IllegalArgumentException("[ERROR] 주문 수량은 20개 초과하면 안됩니다");
+        }
+    }
+
+    private void validateDuplication(List<OrderItem> orderItems) {
+        boolean hasDuplicationOrderItem = orderItems.stream()
+                .distinct()
+                .toList()
+                .size() != orderItems.size();
+        if (hasDuplicationOrderItem) {
+            throw new IllegalArgumentException("[ERROR] 중복된 제품을 입력하면 안됩니다");
+        }
+    }
+
+    private void validateOnlyDrinkMenu(List<OrderItem> orderItems) {
+
+        boolean allDrinkMenu = orderItems.stream()
+                .allMatch(OrderItem::isDrinkMenu);
+        if (allDrinkMenu) {
+            throw new IllegalArgumentException("[ERROR] 음료만 주문하시면 안됩니다");
+        }
+
+    }
+
+
+
+
+}
