c3a1bfb845daefe2d04003f4374e2acacf4fd483
JuSeong1130 <jsmoon1130@naver.com>
2023-11-12 17:31:55 +0900

    test(OrderItemTest) : 드링크메뉴인지 확인 기능 테스트

--- src/test/java/christmas/model/order/OrderItemTest.java
+++ src/test/java/christmas/model/order/OrderItemTest.java
@@ -3,6 +3,7 @@ package christmas.model.order;
 import static org.junit.jupiter.api.Assertions.*;
 
 import christmas.model.menu.Menu;
+import christmas.model.menu.MenuCategory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,5 +33,36 @@ class OrderItemTest {
         // when then
         Assertions.assertThatCode(() -> new OrderItem(menu, quantity))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("입력된 메뉴가 드링크 메뉴라면 True")
+    void 드링크_메뉴_True() {
+        // given
+        String drinkName = "제로콜라";
+        int price = 3000;
+        Menu menu = new Menu(drinkName, price);
+        OrderItem orderItem = new OrderItem(menu, price);
+        // when
+        boolean isDrinkMenu = orderItem.isDrinkMenu();
+
+        //then
+        Assertions.assertThat(isDrinkMenu).isTrue();
+    }
+
+    @Test
+    @DisplayName("입력된 메뉴가 드링크 메뉴가 아니라면 False")
+    void 드링크_메뉴_False() {
+        // given
+        String drinkName = "드링크아님";
+        int price = 3000;
+        Menu menu = new Menu(drinkName, price);
+        OrderItem orderItem = new OrderItem(menu, price);
+
+        // when
+        boolean isDrinkMenu = orderItem.isDrinkMenu();
+
+        //then
+        Assertions.assertThat(isDrinkMenu).isFalse();
     }
 }
