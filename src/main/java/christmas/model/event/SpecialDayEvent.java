26e59f35d6b6958f7f96d9cfd63713f9de49afaa
JuSeong1130 <jsmoon1130@naver.com>
2023-11-14 00:40:17 +0900

    feat(SpecialDayEvent) : SpecialDay 할인 금액 구하기 기능 구현

---
+++ src/main/java/christmas/model/event/SpecialDayEvent.java
@@ -0,0 +1,23 @@
+package christmas.model.event;
+
+import christmas.model.order.Order;
+import java.time.LocalDate;
+import java.util.List;
+
+public class SpecialDayEvent implements Event {
+
+    private static final String SPECIAL_EVENT = "특별 할인";
+
+    @Override
+    public DiscountResult calculateDiscountAmount(Order order) {
+        long discountAmount = 0;
+        LocalDate startTime = LocalDate.of(2023, 12, 1);
+        LocalDate endTime = LocalDate.of(2023, 12, 25);
+
+        if(order.isSpecialDay() && order.isTotalAmountAboveThan(10000) && order.isTimeInRange(startTime, endTime)) {
+            discountAmount = 1000;
+        }
+        return new DiscountResult(SPECIAL_EVENT, discountAmount);
+
+    }
+}
