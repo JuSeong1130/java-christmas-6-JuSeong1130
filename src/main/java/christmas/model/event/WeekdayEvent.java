dd01fbe74a78b3fcfe36362cb065adf79b38c267
JuSeong1130 <jsmoon1130@naver.com>
2023-11-14 00:37:49 +0900

    feat(WeekdayEvent) : 평일 이벤트 할인 금액 구하기 기능 구현

---
+++ src/main/java/christmas/model/event/WeekdayEvent.java
@@ -0,0 +1,22 @@
+package christmas.model.event;
+
+import christmas.model.menu.MenuType;
+import christmas.model.order.Order;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class WeekdayEvent implements Event{
+    private static final String WEEKDAY_EVENT = "평일 할인";
+    @Override
+    public DiscountResult calculateDiscountAmount(Order order) {
+
+        long discountAmount = 0;
+        LocalDate startTime = LocalDate.of(2023, 12, 1);
+        LocalDate endTime = LocalDate.of(2023, 12, 31);
+
+        if(order.isWeekday() && order.isTotalAmountAboveThan(10000) && order.isTimeInRange(startTime, endTime)) {
+            discountAmount = order.countMenuType(MenuType.DESSERT) * 2023;
+        }
+        return new DiscountResult(WEEKDAY_EVENT, discountAmount);
+    }
+}
