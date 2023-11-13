54486879e05aaa829085c0b64b7389f53aff5049
JuSeong1130 <jsmoon1130@naver.com>
2023-11-14 00:42:15 +0900

    feat(ChristmasEvent) : 크리스마스 이벤트 할인 금액 기능 구하기 기능 구현

---
+++ src/main/java/christmas/model/event/ChristmasEvent.java
@@ -0,0 +1,23 @@
+package christmas.model.event;
+
+import christmas.model.menu.MenuType;
+import christmas.model.order.Order;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ChristmasEvent implements Event{
+
+    private static final String CHRISTMAS_EVENT = "크리스마스 디데이 할인";
+    @Override
+    public DiscountResult calculateDiscountAmount(Order order) {
+        long discountAmount = 0;
+
+        LocalDate startTime = LocalDate.of(2023, 12, 1);
+        LocalDate endTime = LocalDate.of(2023, 12, 25);
+
+        if(order.isTotalAmountAboveThan(10000) && order.isTimeInRange(startTime, endTime)) {
+            discountAmount = (order.calculateDaysSinceEventStart(startTime) * 100) + 1000;
+        }
+        return new DiscountResult(CHRISTMAS_EVENT, discountAmount);
+    }
+}
