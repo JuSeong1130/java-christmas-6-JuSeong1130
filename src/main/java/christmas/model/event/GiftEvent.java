be3b530a63cdcd38b042f7439eb7977c9e62b5b1
JuSeong1130 <jsmoon1130@naver.com>
2023-11-13 23:45:31 +0900

    feat(GiftEvent) : 할인 금액 기능 구현

---
+++ src/main/java/christmas/model/event/GiftEvent.java
@@ -0,0 +1,20 @@
+package christmas.model.event;
+
+import christmas.model.order.Order;
+import java.time.LocalDate;
+
+public class GiftEvent implements Event{
+    private static final String GIFT_EVENT = "증정 이벤트";
+
+    @Override
+    public DiscountResult calculateDiscountAmount(Order order) {
+        long discountAmount = 0;
+        LocalDate startTime = LocalDate.of(2023, 12, 1);
+        LocalDate endTime = LocalDate.of(2023, 12, 31);
+
+        if(order.isTotalAmountAboveThan(120000) && order.isTimeInRange(startTime, endTime)) {
+            discountAmount = Gift.findGiftAmountBy(order.totalPurchaseAmount());
+        }
+        return new DiscountResult(GIFT_EVENT, discountAmount);
+    }
+}
