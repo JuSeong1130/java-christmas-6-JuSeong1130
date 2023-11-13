464ad70b01a814c1888121d92c37be7aa6909944
JuSeong1130 <jsmoon1130@naver.com>
2023-11-14 00:30:03 +0900

    test(OrderTest) : Order가 평일인지 확인하는 기능 테스트

--- src/test/java/christmas/model/order/OrderTest.java
+++ src/test/java/christmas/model/order/OrderTest.java
@@ -280,6 +280,54 @@ class OrderTest {
         Assertions.assertThat(expected).isFalse();
     }
 
+    @Test
+    @DisplayName("Order의 주문일자가 평일이라면 True")
+    void 주문일자_평일_True() {
+        String name = "제로콜라";
+        int price = 3000;
+        int quantity = 1;
+        Menu menu = new Menu(MenuType.DRINK, name, price);
+
+        String otherName= "티본스테이크";
+        int otherPrice = 55000;
+        int otherQuantity = 2;
+        Menu otherMenu = new Menu(MenuType.MAIN_COURSE, otherName, otherPrice);
+
+        OrderItem orderItem = new OrderItem(menu, quantity);
+        OrderItem otherOrderItem = new OrderItem(otherMenu, otherQuantity);
+        List<OrderItem> orderItems = List.of(orderItem, otherOrderItem);
+        Order order = new Order(orderItems, LocalDate.of(2023, 12 , 21));
+
+        //when
+        boolean expected = order.isWeekday();
+
+        //then
+        Assertions.assertThat(expected).isTrue();
+    }
+    @Test
+    @DisplayName("Order의 주문일자가 평일 아니라면 False")
+    void 주문일자_평일_아닐_시_False() {
+        String name = "제로콜라";
+        int price = 3000;
+        int quantity = 1;
+        Menu menu = new Menu(MenuType.DRINK, name, price);
+
+        String otherName= "티본스테이크";
+        int otherPrice = 55000;
+        int otherQuantity = 2;
+        Menu otherMenu = new Menu(MenuType.MAIN_COURSE, otherName, otherPrice);
+
+        OrderItem orderItem = new OrderItem(menu, quantity);
+        OrderItem otherOrderItem = new OrderItem(otherMenu, otherQuantity);
+        List<OrderItem> orderItems = List.of(orderItem, otherOrderItem);
+        Order order = new Order(orderItems, LocalDate.of(2023, 12 , 22));
+
+        //when
+        boolean expected = order.isWeekday();
+
+        //then
+        Assertions.assertThat(expected).isFalse();
+    }
 
 
 }
