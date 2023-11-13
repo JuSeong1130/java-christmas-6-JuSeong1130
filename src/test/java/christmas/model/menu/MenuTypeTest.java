ee9034c8a04bdcefb322e3dce5f6def1dd73e8fa
JuSeong1130 <jsmoon1130@naver.com>
2023-11-13 23:37:10 +0900

    refactor(MenuTypeTest) : menuType 구조 변경으로 인한 테스트 기능 리팩토링

--- src/test/java/christmas/model/menu/MenuTypeTest.java
+++ src/test/java/christmas/model/menu/MenuTypeTest.java
@@ -1,23 +1,18 @@
 package christmas.model.menu;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 
-class MenuCategoryTest {
+class MenuTypeTest {
 
     @Test
     @DisplayName("입력된 메뉴가 드링크 메뉴라면 True")
     void 드링크_메뉴_True() {
         // given
-        String drinkName = "제로콜라";
-        int price = 3000;
-        Menu menu = new Menu(drinkName, price);
+        MenuType menuType = MenuType.DRINK;
         // when
-        boolean isDrinkMenu = MenuCategory.isDrinkMenu(menu);
+        boolean isDrinkMenu = menuType.isDrinkMenu();
 
         //then
         Assertions.assertThat(isDrinkMenu).isTrue();
@@ -27,11 +22,10 @@ class MenuCategoryTest {
     @DisplayName("입력된 메뉴가 드링크 메뉴가 아니라면 False")
     void 드링크_메뉴_False() {
         // given
-        String drinkName = "드링크아님";
-        int price = 3000;
-        Menu menu = new Menu(drinkName, price);
+        MenuType menuType = MenuType.MAIN_COURSE;
+
         // when
-        boolean isDrinkMenu = MenuCategory.isDrinkMenu(menu);
+        boolean isDrinkMenu = menuType.isDrinkMenu();
 
         //then
         Assertions.assertThat(isDrinkMenu).isFalse();
