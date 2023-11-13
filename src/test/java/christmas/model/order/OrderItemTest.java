07bbf5867a76537e3d08179cba1889e5ce90981e
JuSeong1130 <jsmoon1130@naver.com>
2023-11-13 23:52:52 +0900

    test(OrderItemTest) : 주문항목이 메뉴 타입과 일치하는지 기능 테스트

--- src/test/java/christmas/model/order/OrderItemTest.java
+++ src/test/java/christmas/model/order/OrderItemTest.java
@@ -1,13 +1,11 @@
 package christmas.model.order;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import christmas.model.menu.DrinkMenu;
 import christmas.model.menu.Menu;
-import christmas.model.menu.MenuCategory;
+import christmas.model.menu.MenuType;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 
 class OrderItemTest {
 
@@ -16,7 +14,7 @@ class OrderItemTest {
     void OrderItem_생성_예외_테스트() {
         // given
         int quantity = 0;
-        Menu menu = new Menu("테스트", 0);
+        Menu menu = new Menu(MenuType.DRINK, "테스트", 0);
 
         // when then
         Assertions.assertThatThrownBy(() -> new OrderItem(menu, quantity))
@@ -28,7 +26,7 @@ class OrderItemTest {
     void OrderItem_생성_성공_테스트() {
         // given
         int quantity = 1;
-        Menu menu = new Menu("테스트", 0);
+        Menu menu = new Menu(MenuType.DRINK, "테스트", 0);
 
         // when then
         Assertions.assertThatCode(() -> new OrderItem(menu, quantity))
@@ -41,7 +39,7 @@ class OrderItemTest {
         // given
         String drinkName = "제로콜라";
         int price = 3000;
-        Menu menu = new Menu(drinkName, price);
+        Menu menu = new Menu(MenuType.DRINK, drinkName, price);
         OrderItem orderItem = new OrderItem(menu, price);
         // when
         boolean isDrinkMenu = orderItem.isDrinkMenu();
@@ -56,7 +54,7 @@ class OrderItemTest {
         // given
         String drinkName = "드링크아님";
         int price = 3000;
-        Menu menu = new Menu(drinkName, price);
+        Menu menu = new Menu(MenuType.MAIN_COURSE, drinkName, price);
         OrderItem orderItem = new OrderItem(menu, price);
 
         // when
@@ -64,5 +62,39 @@ class OrderItemTest {
 
         //then
         Assertions.assertThat(isDrinkMenu).isFalse();
+    }
+
+    @Test
+    @DisplayName("주문 항목의 메뉴타입과 일치한다면 True")
+    void 주문항목_메뉴타입_일치_True() {
+        // given
+        String drinkName = DrinkMenu.CHAMPAGNE.getMenuName();
+        int price = DrinkMenu.CHAMPAGNE.getPrice();
+        MenuType menuType = MenuType.DRINK;
+        Menu menu = new Menu(menuType, drinkName, price);
+        OrderItem orderItem = new OrderItem(menu, price);
+
+        // when
+        boolean isEqualMenuType = orderItem.isEqualMenuType(menuType);
+
+        // then
+        Assertions.assertThat(isEqualMenuType).isTrue();
+    }
+
+    @Test
+    @DisplayName("주문 항목의 메뉴타입과 일치하지 않는다면 False")
+    void 주문항목_메뉴타입_일치X_False() {
+        // given
+        String drinkName = DrinkMenu.CHAMPAGNE.getMenuName();
+        int price = DrinkMenu.CHAMPAGNE.getPrice();
+        MenuType menuType = MenuType.DRINK;
+        Menu menu = new Menu(menuType, drinkName, price);
+        OrderItem orderItem = new OrderItem(menu, price);
+
+        // when
+        boolean isEqualMenuType = orderItem.isEqualMenuType(MenuType.MAIN_COURSE);
+
+        // then
+        Assertions.assertThat(isEqualMenuType).isFalse();
     }
 }
