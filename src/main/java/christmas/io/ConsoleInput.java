4c2577bf5904f13c6accb0a115e7df5fa439293e
JuSeong1130 <jsmoon1130@naver.com>
2023-11-14 18:25:54 +0900

    style(ConsoleInput) : 불필요한 띄어쓰기 삭제

--- src/main/java/christmas/io/ConsoleInput.java
+++ src/main/java/christmas/io/ConsoleInput.java
@@ -2,7 +2,7 @@ package christmas.io;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class ConsoleInput implements Input{
+public class ConsoleInput implements Input {
 
     @Override
     public String readLine() {
@@ -12,16 +12,12 @@ public class ConsoleInput implements Input{
     }
 
     private void validate(String str) {
-
-        if(str == null) {
+        if (str == null) {
             throw new IllegalArgumentException("[ERROR] 값을 입력 하셔야 합니다.");
         }
 
-        if(str.isBlank()) {
+        if (str.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 값을 입력하셔야 합니다.");
         }
     }
-
-
-
 }
