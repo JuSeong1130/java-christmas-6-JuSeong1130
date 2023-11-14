2ddcad1cfb0941d4638ac27535c110c0d6ac5df3
JuSeong1130 <jsmoon1130@naver.com>
2023-11-14 19:53:51 +0900

    test(OrderQuantity) : OrderQuantity 생성 테스트

--- src/test/java/christmas/model/order/OrderItemTest.java
+++ src/test/java/christmas/model/order/OrderItemTest.java
@@ -10,30 +10,6 @@ import org.junit.jupiter.api.Test;
 class OrderItemTest {
 
     @Test
-    @DisplayName("주문한 수량이 1개 미만이라면 예외 발생")
-    void OrderItem_생성_예외_테스트() {
-        // given
-        int quantity = 0;
-        Menu menu = new Menu(MenuType.DRINK, "테스트", 0);
-
-        // when then
-        Assertions.assertThatThrownBy(() -> new OrderItem(menu, new OrderQuantity(quantity)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("주문한 수량이 1개 이상이라면 예외 없이 생성 성공")
-    void OrderItem_생성_성공_테스트() {
-        // given
-        int quantity = 1;
-        Menu menu = new Menu(MenuType.DRINK, "테스트", 0);
-
-        // when then
-        Assertions.assertThatCode(() -> new OrderItem(menu, new OrderQuantity(quantity)))
-                .doesNotThrowAnyException();
-    }
-
-    @Test
     @DisplayName("입력된 메뉴가 드링크 메뉴라면 True")
     void 드링크_메뉴_True() {
         // given
