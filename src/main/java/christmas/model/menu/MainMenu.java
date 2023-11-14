3a3db122bcdebda709b881cbbf111dc4a620a402
JuSeong1130 <jsmoon1130@naver.com>
2023-11-14 18:23:16 +0900

    fix(MainMenu) : 바베큐립이 아닌 바비큐립으로 수정

--- src/main/java/christmas/model/menu/MainMenu.java
+++ src/main/java/christmas/model/menu/MainMenu.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public enum MainMenu {
 
     T_BONE_STEAK("티본스테이크", 55000),
-    BARBECUE_RIBS("바베큐립", 54000),
+    BARBECUE_RIBS("바비큐립", 54000),
     SEAFOOD_PASTA("해산물파스타", 35000),
     CHRISTMAS_PASTA("크리스마스파스타", 25000);
 
