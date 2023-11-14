3afd3b2e507d0bba68d4c0f9ccfb9ab9010bc762
JuSeong1130 <jsmoon1130@naver.com>
2023-11-14 22:34:42 +0900

    feat(OrderService) : expecteOrderResult 결과 구하는 기능 구현

---
+++ src/main/java/christmas/service/OrderService.java
@@ -0,0 +1,25 @@
+package christmas.service;
+
+import christmas.config.AppConfig;
+import christmas.controller.dto.RequestOrderItemDto;
+import christmas.model.event.DiscountResult;
+import christmas.model.event.DiscountResults;
+import christmas.model.event.EventPlanner;
+import christmas.model.order.Order;
+import christmas.model.order.OrderItems;
+import christmas.util.OrderMapper;
+import java.time.LocalDate;
+import java.util.List;
+
+public class OrderService {
+
+    public OrderSummaryDto calculateExpectedOrder(LocalDate selectedDate, List<RequestOrderItemDto> orderItemDtos) {
+        OrderItems orderItems = OrderMapper.toOrderItems(orderItemDtos);
+        Order order = new Order(orderItems, selectedDate);
+
+        EventPlanner eventPlanner = AppConfig.createEventPlanner();
+        DiscountResults discountResults = eventPlanner.calculateDiscountAmount(order);
+
+        return OrderMapper.toOrderSummaryDto(order, discountResults);
+    }
+}
