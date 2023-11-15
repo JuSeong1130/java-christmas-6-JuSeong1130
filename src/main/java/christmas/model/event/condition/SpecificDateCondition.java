38496a018cabd4cc22fa35fb968df374957787c8
JuSeong1130 <jsmoon1130@naver.com>
2023-11-15 22:17:11 +0900

    feat(SpecificDateCondition) : 조건이 유효한지 기능 구현

---
+++ src/main/java/christmas/model/event/condition/SpecificDateCondition.java
@@ -0,0 +1,18 @@
+package christmas.model.event.condition;
+
+import christmas.model.order.Order;
+import java.util.List;
+
+public class SpecificDateCondition implements EventCondition{
+
+    private final List<Integer> days;
+
+    public SpecificDateCondition(List<Integer> days) {
+        this.days = days;
+    }
+
+    @Override
+    public boolean isValidCondition(Order order) {
+        return order.isDateIncludedIn(days);
+    }
+}
