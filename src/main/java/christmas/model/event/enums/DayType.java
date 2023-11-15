871053530a2992e2e9ed993badf55f0a9ef586ac
JuSeong1130 <jsmoon1130@naver.com>
2023-11-15 22:18:32 +0900

    feat(DayType) : Day가 주말인지 평일인지 타입 선언

---
+++ src/main/java/christmas/model/event/enums/DayType.java
@@ -0,0 +1,13 @@
+package christmas.model.event.enums;
+
+public enum DayType {
+
+    WEEKEND("WEEKEND"),
+    WEEKDAY("WEEKDAY")
+    ;
+    private String name;
+
+    DayType(String name) {
+        this.name = name;
+    }
+}
